6ac23358962ef95a86bc02f3520a20ee782fa1c5
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-04-12 23:27:22 -0300

    Entity "Movie" created

---
+++ MoviesDemoAPI/Entities/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MoviesDemoAPI.Entities
+{
+    [Table("tb_movies")]
+    public class Movie
+    {
+        [Key]
+        [Column("movie_id")]
+        public int Id { get; set; }
+        [Column("movie_name")]
+        [StringLength(20)]
+        public string Name { get; set; }
+        [Column("movie_synopsis")]
+        [StringLength(60)]
+        public string Synopsis { get; set; }
+    }
+}
